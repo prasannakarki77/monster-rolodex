81d12b4f743cb9a4012ed15d469c114f117dcd8f
prasannakarki77 <prasanna.karki77@gmail.com>
2022-08-12 20:34:45 +0545

    converted external comp to func comp

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,17 @@
-import { Component } from "react";
-
 import "./card.style.css";
 
-class Card extends Component {
-  render() {
-      const {id,name,email} = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <h2>{email}</h2>
-      </div>
-    );
-  }
-}
+const Card = (props) => {
+  const { id, name, email } = props.monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <h2>{email}</h2>
+    </div>
+  );
+};
 
 export default Card;
